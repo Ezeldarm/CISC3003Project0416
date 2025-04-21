6be4ae7d2f831587e7f5fe128aa2b0ad77d116da
Oliver <sharpstone4848@gmail.com>
2025-04-22 02:43:53 +0800

    fix responsibility (a little bit messed up)

--- home/header.php
+++ home/header.php
@@ -11,18 +11,55 @@
         
             <div class="navbar-wrapper">
             
-            <div class="topnav">
-                <div class="search-container">
-                    <form action="">
-                        <div class="search-wrapper">
-                            <input type="text" placeholder="Search..." name="search">
-                            <button type="submit" class="search-btn" aria-label="Search">
-                                <img src="./assets/images/magnifying-glass.png" alt="搜尋" class="search-icon">
-                            </button>
-                        </div>
-                    </form>
+                <div class="topnav">
+                    <div class="search-container">
+                        <form action="">
+                            <div class="search-wrapper">
+                                <input type="text" placeholder="Search..." name="search">
+                                <button type="submit" class="search-btn" aria-label="Search">
+                                    <img src="./assets/images/magnifying-glass.png" alt="搜尋" class="search-icon">
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
+
+                <div class="mobile-sidebar">
+                    <nav class="side-nav">
+                        <ul class="nav-list">
+                            <li class="nav-item">
+                                <a href="index.php" class="nav-link <?php echo $current_page == 'index.php' ? 'active' : ''; ?>">
+                                    <span class="material-symbols-rounded">home</span>
+                                    <span class="nav-text">Home</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="cities.php" class="nav-link <?php echo $current_page == 'cities.php' ? 'active' : ''; ?>">
+                                    <span class="material-symbols-rounded">location_city</span>
+                                    <span class="nav-text">Cities</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="mylist.php" class="nav-link <?php echo $current_page == 'mylist.php' ? 'active' : ''; ?>">
+                                    <span class="material-symbols-rounded">list</span>
+                                    <span class="nav-text">My List</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="mypost.php" class="nav-link <?php echo $current_page == 'mypost.php' ? 'active' : ''; ?>">
+                                    <span class="material-symbols-rounded">post</span>
+                                    <span class="nav-text">My Post</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="profile.php" class="nav-link <?php echo $current_page == 'profile.php' ? 'active' : ''; ?>">
+                                    <span class="material-symbols-rounded">person</span>
+                                    <span class="nav-text">My Profile</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
                 <?php 
                 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true): ?>
